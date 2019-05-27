7498fdecc44944b3b3d62165b814232b23c91f40
Augusto <augustomesquitasrs@gmail.com>
2019-05-27 14:00:02 -0300

    Atualizado lista de campeoes.

--- src/app/dialog-match/dialog-match.component.ts
+++ src/app/dialog-match/dialog-match.component.ts
@@ -48,7 +48,7 @@ export class DialogMatchComponent implements OnInit {
   ngOnInit() {
     this.http
       .get<any>(
-        'http://ddragon.leagueoflegends.com/cdn/9.3.1/data/pt_BR/champion.json'
+        'http://ddragon.leagueoflegends.com/cdn/9.10.1/data/pt_BR/champion.json'
       )
       .subscribe(data => {
         this.championList = Object.keys(data.data);
